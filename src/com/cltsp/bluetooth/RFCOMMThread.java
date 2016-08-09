54fac5ab1582bdd6cfc17237a068cc3aeda510b1
leacher <zclzcllove@gmail.com>
2016-08-09 19:49:59 +0800

    rest sppclient.java

---
+++ src/com/cltsp/bluetooth/RFCOMMThread.java
@@ -0,0 +1,19 @@
+package com.cltsp.bluetooth;
+
+ /*
+ * *************************************
+ * Created by leacher on 16-8-3.
+ * *************************************
+ * spp(Serial Port Profile) 协议连接线程
+ * 拿到一个远程设备实例去尝试spp协议连接
+ * *************************************
+ */
+
+public class RFCOMMThread implements Runnable{
+
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/com/cltsp/util/DynamicProxyHandler.java
@@ -0,0 +1,7 @@
+package com.cltsp.util;
+
+/**
+ * Created by leacher on 16-8-9.
+ */
+public class DynamicProxyHandler {
+}
